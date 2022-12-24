d89325da9e6cf472d0fa3ab27de957531738b798
Davron <davron>
2022-12-25 00:02:12 +0500

    fourth commit

--- script.js
+++ script.js
@@ -58,34 +58,34 @@ let students = {
         }
 };
 
-// 1)
-function getTotalProgressByIteration (data) {
-    let total = 0;
-    let students = 0;
 
-    for (let course of Object.values(data)) {
-        if (Array.isArray(course)){
-            students += course.length;
+// function getTotalProgressByIteration (data) {
+//     let total = 0;
+//     let students = 0;
+
+//     for (let course of Object.values(data)) {
+//         if (Array.isArray(course)){
+//             students += course.length;
             
-        for(let i = 0; i < course.length; i++){
-            total += course[i].progress;
-            }
-        } else {
-            for (let subCourse of Object.values(course)) {
-                students += subCourse.length;
+//         for(let i = 0; i < course.length; i++){
+//             total += course[i].progress;
+//             }
+//         } else {
+//             for (let subCourse of Object.values(course)) {
+//                 students += subCourse.length;
 
-                for(let i = 0; i < subCourse.length; i++){
-                    total += subCourse[i].progress;
-                }
-            }
-        }
-    }
-    return total / students;
-} 
-// // console.log(getTotalProgressByIteration(students));
+//                 for(let i = 0; i < subCourse.length; i++){
+//                     total += subCourse[i].progress;
+//                 }
+//             }
+//         }
+//     }
+//     return total / students;
+// } 
+// console.log(getTotalProgressByIteration(students));
 
 
-// // 2)
+// // // 2)
 function getTotalProgressByRecursion(data) {
     if (Array.isArray(data)){
         let total = 0;
@@ -108,67 +108,23 @@ const result = getTotalProgressByRecursion(students);
 console.log(result[0]/result[1]);
 
 
-// Пример для Object.values()
-
-// let person = {
-//     name: 'Davron',
-//     age: 35,
-//     weight: 67,
-//     height: 178
-// };
-
-// console.log(Object.values(person));
 
 
-// Объявление функции(Function Declaration)
-// Рекурсия
 
-// function getSum(numOne, numTwo) {
-//     let result = 1;
-//     //умножаем result на numOne numTwo раз в цикле
-//     for(let i = 0; i < numTwo; i++) {
-//         result *= numOne;
-//     }  
-//     return result;
-// }
 
-// console.log(getSum(2, 3));
 
-// function getSum(numOne, numTwo) {
-//     if (numTwo == 1) {
-//         return numOne;
-//     } else {
-//         return numOne * getSum(numOne, numTwo - 1);
-//     }
-// }
-// console.log(getSum(2, 3));
 
-let count = 0;
-function recursion() {
-    if (count === 5) {
-        return 'Done'; 
-    }
-    count++;
-    return recursion();
-}
 
-console.log(recursion());
 
-let number = Math.pow(3, 5);
-console.log(number);
 
-function pow(x, y) {
-    if (y === 0) {
-        return 1;
-    } else {
-        return x * pow(x, y - 1);
-    }
-}
 
-console.log(pow(2, 2));
-console.log(pow(2, 3));
-console.log(pow(2, 4));
-console.log(pow(3, 2));
+
+
+
+
+
+
+
 
 
 
